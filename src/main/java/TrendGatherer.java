268c403ad0b770eb619f95464bef6d10ad178526
Jordi Ricard Onrubia Palacios <jordirop@Jordis-MacBook-Air.local>
2020-05-10 22:20:02 +0200

    using hadoop regex mapper instead of own mapper

--- src/main/java/TrendGatherer.java
+++ src/main/java/TrendGatherer.java
@@ -1,12 +1,14 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.chain.ChainMapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.map.RegexMapper;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
@@ -15,23 +17,8 @@ import java.util.regex.Pattern;
 
 public class TrendGatherer {
 
-    public static class TopicMapper extends Mapper<Object, Text, Text, IntWritable>{
-        final static Pattern PATTERN = Pattern.compile("(?:\\s|\\A|^)[#]+([A-Za-z0-9-_]+)");
-        private final static IntWritable one = new IntWritable(1);
-        private final Text word = new Text();
-
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            Matcher patternMatcher = PATTERN.matcher(value.toString());
-            while(patternMatcher.find()) {
-                String occurrence = patternMatcher.group();
-                word.set(occurrence);
-                context.write(word, one);
-            }
-        }
-    }
-
     public static class CleanUpMapper extends Mapper<Text, IntWritable, Text, IntWritable>{
-        final static Pattern PATTERN = Pattern.compile("(?:\\s|\\A|^)[#]+([A-Za-z0-9-_]+)");
+        final static Pattern PATTERN = Pattern.compile("(?:\\s|\\A|^)[##]+([A-Za-z0-9-_]+)");
         private final static IntWritable one = new IntWritable(1);
         private final Text word = new Text();
 
@@ -45,12 +32,12 @@ public class TrendGatherer {
         }
     }
 
-    public static class CounterReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
-        private final IntWritable result = new IntWritable();
-        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+    public static class CounterReducer extends Reducer<Text,LongWritable,Text,LongWritable> {
+        private final LongWritable result = new LongWritable();
+        public void reduce(Text key, Iterable<LongWritable> values, Context context)
                 throws IOException, InterruptedException {
             int sum = 0;
-            for (IntWritable val : values) {
+            for (LongWritable val : values) {
                 sum += val.get();
             }
             result.set(sum);
@@ -59,15 +46,16 @@ public class TrendGatherer {
     }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        String PATTERN = "(?:\\s|\\A|^)[##]+([A-Za-z0-9-_]+)";
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "Trending Gatherer");
         job.setJarByClass(TrendGatherer.class);
 
         // Topic Mapper
         Configuration topicMapperConf = new Configuration(false);
-        ChainMapper.addMapper(job, TopicMapper.class, Object.class, Text.class, Text.class,
-                IntWritable.class, topicMapperConf);
-
+        topicMapperConf.set(RegexMapper.PATTERN, PATTERN);
+        ChainMapper.addMapper(job, RegexMapper.class, Object.class, Text.class, Text.class,
+                LongWritable.class, topicMapperConf);
         job.setReducerClass(CounterReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
